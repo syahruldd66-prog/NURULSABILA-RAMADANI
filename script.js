8e10c56eeb24c4a71e7087ef2038245c6097ecb1
syahruldd66-prog <syahrul.dd66@gmail.com>
2026-05-06 09:26:13 +0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,12 @@
+// Contoh script sederhana untuk efek scroll halus
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+console.log("Portfolio Nurul Sabila Ramadani siap!");
+
